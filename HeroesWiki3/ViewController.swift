ff1be8bc992b2fa7cc33c586f8f9165dec5ed601
Maxim Mitin <maximus405@gmail.com>
2021-07-31 19:34:50 +0300

    Initial Commit

---
+++ HeroesWiki3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroesWiki3
+//
+//  Created by Maxim Mitin on 31.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
