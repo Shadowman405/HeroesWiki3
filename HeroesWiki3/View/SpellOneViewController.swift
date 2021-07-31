04c14fd226d2bde2a7245e04940e881ce1bd35d1
Maxim Mitin <maximus405@gmail.com>
2021-08-01 02:39:17 +0300

    Spells first lvl +

---
+++ HeroesWiki3/View/SpellOneViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SpellOneViewController.swift
+//  HeroesWiki3
+//
+//  Created by Maxim Mitin on 1.08.21.
+//
+
+import UIKit
+
+class SpellOneViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var spellData = spellsFirstLevel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        spellData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "spellCell", for: indexPath) as? SpellsFirstLvlTableViewCell {
+            cell.configCell(spells: spellData[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+}
--- HeroesWiki3/View/SpellsFirstLvlTableViewCell.swift
+++ HeroesWiki3/View/SpellsFirstLvlTableViewCell.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class SpellsFirstLvlTableViewCell: UITableViewCell {
+    @IBOutlet weak var spellImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var manaCoastLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
     
-
+    var firstLvlSpells = spellsFirstLevel
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        //townImage.layer.cornerRadius = 10
+        spellImage.layer.cornerRadius = 30
     }
 
+    func configCell(spells: Spell) {
+        spellImage.image = UIImage(named: spells.spellImage)
+        nameLbl.text = spells.name
+        manaCoastLbl.text = "Mana coast: \(spells.manaCost)"
+        descriptionLbl.text = "Type - \(spells.type).\nDescription: \(spells.description)"
+    }
 }
